b3a31255789076814c7e90a833f7e51e397542ab
Danilo-luz <daniloaugustoestudo@gmail.com>
2024-11-21 17:39:04 -0300

    Mobile version

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+function redirectLogin(){
+    window.location.href = "login.html";
+}
+
+function redirectInstrucao(){
+    window.location.href = "comojogar.html";
+}
+
+function redirectHistoria(){
+    window.location.href = "historia.html";
+}
+
+function voltarInicio(){
+    window.location.href = "index.html";
+}
+
+function informarFuncao(){
+    window.alert('Função não implementada!')
+}
